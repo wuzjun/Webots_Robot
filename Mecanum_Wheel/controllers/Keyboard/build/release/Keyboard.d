d658de5e58df4b5dfa082104db3618c8aae0eb19
WNKAGA <1159904119@qq.com>
2020-12-03 20:05:41 +0800

    创建了麦轮小车

---
+++ Mecanum_Wheel/controllers/Keyboard/build/release/Keyboard.d
@@ -0,0 +1,6 @@
+build/release/Keyboard.o: Keyboard.c \
+ E:\Program\ Files\Webots/include/controller/c/webots/robot.h \
+ E:\Program\ Files\Webots/include/controller/c/webots/types.h \
+ E:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
+ E:\Program\ Files\Webots/include/controller/c/webots/keyboard.h \
+ E:\Program\ Files\Webots/include/controller/c/webots/motor.h
--- Webots_Poject_1/controllers/epuck_avoid_collision/build/release/epuck_avoid_collision.d
+++
@@ -1,6 +0,0 @@
-build/release/epuck_avoid_collision.o: epuck_avoid_collision.c \
- E:\Program\ Files\Webots/include/controller/c/webots/robot.h \
- E:\Program\ Files\Webots/include/controller/c/webots/types.h \
- E:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
- E:\Program\ Files\Webots/include/controller/c/webots/distance_sensor.h \
- E:\Program\ Files\Webots/include/controller/c/webots/motor.h
