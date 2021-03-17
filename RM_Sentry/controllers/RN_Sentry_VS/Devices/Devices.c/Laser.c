2d35605db8f31d3982a2be6ef9faceba2d7cc7b1
WNKAGA <1159904119@qq.com>
2021-03-17 20:20:12 +0800

    哨兵加上了两个侧面的测距，代码也补全了，创建了一个小车的控制器

--- RM_Sentry/controllers/RN_Sentry_VS/Devices/Devices.c/Laser.c
+++ RM_Sentry/controllers/RN_Sentry_VS/Devices/Devices.c/Laser.c
@@ -7,17 +7,30 @@
  */
 #include <Laser.h>
 
-WbDeviceTag laser0;
-double distance;
+WbDeviceTag laser[3];
+double distance[3];
 
 void Laser_Init(int TIME_STEP)
 {
-	laser0 = wb_robot_get_device("laser0");
-	wb_distance_sensor_enable(laser0, TIME_STEP);
+	char lasters_names[3][8] = {
+		"laser0", "laser1", "laser2" };
+
+	for (int i = 0; i < 3; i++)
+	{
+		laser[i] = wb_robot_get_device(lasters_names[i]);
+		wb_distance_sensor_enable(laser[i], TIME_STEP);
+	}
+
 }
 
 void Laser_get_Data(void)
 {
-	distance = wb_distance_sensor_get_value(laser0);
-	//printf("¾àÀëÊÇ£º%lf\n", distance);
+	for (int i = 0; i < 3; i++)
+	{
+		distance[i] = wb_distance_sensor_get_value(laser[i]);
+
+	}
+	printf("distance0 = %lf\n", distance[0]);
+	printf("distance1 = %lf\n", distance[1]);
+	printf("distance2 = %lf\n", distance[2]);
 }
